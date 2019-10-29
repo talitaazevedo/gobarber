414f12060113df475ae789af69dbe48cde6f57a4
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-28 22:11:12 -0300

    Fazendo upload de imagem

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerconfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerconfig);
 
 routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionController.store);
 
 // declaração de forma gglobal.
+// este middleware evita que o usuário  consiga fazer update sem  estar logado
 routes.use(authMiddleware);
+// upload.single('nome do campo que eu vou enviar')  =>
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({ ok: true });
+});
 
 routes.put('/users', UserController.update);
 export default routes;
