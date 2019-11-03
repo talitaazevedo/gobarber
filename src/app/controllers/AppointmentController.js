7c688d4b5af22aa4a4a9d2a1275fdb0e2d14c65d
Talita Azevedo <talita.azevedo360@gmail.com>
2019-11-02 21:20:42 -0300

    Aplicando validaçoes de Agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -24,6 +25,30 @@ class AppointmentController {
         if (!isProvider) {
             return res.status(401).json({
                 error: 'You can only create Appointments with Providers',
+            });
+        }
+        /**
+         * Check for past dates
+         */
+        const hourStart = startOfHour(parseISO(date));
+        if (isBefore(hourStart, new Date())) {
+            return res
+                .status(400)
+                .json({ error: 'Past dates are note permited' });
+        }
+        /**
+         * Check date Availability
+         */
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+        if (checkAvailability) {
+            return res.status(400).json({
+                error: 'Appointment date is not available',
             });
         }
         const appointment = await Appointment.create({
