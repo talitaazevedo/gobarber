69f9617b88f16e02b19cd866fa90a9787f58a01e
Talita Azevedo <talita.azevedo360@gmail.com>
2019-11-02 22:32:09 -0300

    Aplicando a paginaçao

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,10 +6,16 @@ import File from '../models/File';
 
 class AppointmentController {
     async index(req, res) {
+        const { page = 1 } = req.query;
+
         const appointments = await Appointment.findAll({
             where: { user_id: req.userId, canceled_at: null },
             order: ['date'],
             attributes: ['id', 'date'],
+            // listagem limite de 20 em 20
+            limit: 20,
+            // Essa conta é simples mas bem util
+            offset: (page - 1) * 20,
             include: [
                 {
                     model: User,
