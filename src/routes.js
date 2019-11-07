7ea4053c151b3f5f49b786e794fc0b50e1d90996
Talita Azevedo <talita.azevedo360@gmail.com>
2019-11-07 19:07:36 -0300

    Listanddo Notificaçoes do usuario

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 // Import de Middlewares
 import authMiddleware from './app/middlewares/auth';
 
@@ -30,6 +31,7 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
 
 routes.put('/users', UserController.update);
 
