98c2579307eec7e72e8851fcaa94fa799b8313cd
Talita Azevedo <talita.azevedo360@gmail.com>
2019-11-09 18:29:22 -0300

    Configurando a fila com Redis

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
     // lista agendamento
@@ -152,24 +153,8 @@ class AppointmentController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
-        // Configuração de envio de email
-        await Mail.sendMail({
-            to: `${appointment.provider.name}  <${appointment.provider.email} >`,
-            subject: 'Agendamento Cancelado',
-            template: 'cancellation',
-            // envia todas as váriaveis que o cancellation está esperando
-            context: {
-                provider: appointment.provider.name,
-                user: appointment.user.name,
-                date: format(
-                    appointment.date,
-                    "'dia' dd 'de' MMM', às 'H:mm'h'",
-                    {
-                        locale: pt,
-                    }
-                ),
-            },
-        });
+        await Queue.add(CancellationMail.key, { appointment });
+
         return res.json(appointment);
     }
 }
---
+++ src/lib/Queue.js
@@ -0,0 +1,50 @@
+/**
+ *  *** configurações do queue para filas ***
+ *
+ *   *** Uma fila para cada background job ***
+ *
+ *
+ *
+ */
+
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+    constructor() {
+        this.queues = {};
+        this.init();
+    }
+
+    init() {
+        jobs.forEach(({ key, handle }) => {
+            this.queues[key] = {
+                // instancia que conecta com o redis
+                bee: new Bee(key, {
+                    redis: redisConfig,
+                }),
+                // processamento
+                handle,
+            };
+        });
+    }
+
+    add(queue, job) {
+        // Adiciona o job
+        return this.queues[queue].bee.createJob(job).save();
+    }
+
+    processQueue() {
+        // processa o job em tempo real
+        jobs.forEach(job => {
+            const { bee, handle } = this.queues[job.key];
+
+            bee.process(handle);
+        });
+    }
+}
+
+export default new Queue();
